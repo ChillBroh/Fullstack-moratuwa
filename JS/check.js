00c8ae219389494417315156f7d9f187ccdd1eec
Ishara Madusanka <93942615+IT21189944@users.noreply.github.com>
2022-07-10 01:09:56 +0530

    external js files created

---
+++ JS/check.js
@@ -0,0 +1,29 @@
+
+var messages, number;
+
+/*********************************
+try / catch code test
+*********************************/
+function getValue() {
+  messages = document.getElementById("error-message");
+  messages.innerHTML = "";
+  messages.style.color = "red";
+
+  number = document.getElementById("get-value").value;
+
+  try {
+    if (number == "") throw "is Empty";
+    if (isNaN(number)) throw "is not a number";
+    if (number > 10) throw "is too Big";
+    if (number < 5) throw "is too Small";
+
+    debugger;
+
+    if (number == number) {
+      messages.style.color = "green";
+      throw "is " + number;
+    }
+  } catch (err) {
+    messages.innerHTML = "Input " + err;
+  }
+}
