9b2c40c1978868982f200ccacbdd3f54b04dd183
Ishara Madusanka <93942615+IT21189944@users.noreply.github.com>
2022-07-11 23:39:49 +0530

    added java script alert for page

---
+++ Assignment web/JS/newsletter.js
@@ -0,0 +1,4 @@
+function newsletter(){
+    let mail = document.getElementById("email").value;
+    alert("Thank You for Subscribing to our News Letter. Your Email " + mail + " has been successfully added to our Website");
+}
